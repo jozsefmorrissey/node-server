d6fef72abfde088812a402cabba112b11f04cc12
jtmorri2@illinois.com <jtmorri2@illinois.edu>
2019-08-31 21:29:01 -0400

    Debug check before initializing debugGui

--- services/debug-gui/public/js/debug-gui.js
+++ services/debug-gui/public/js/debug-gui.js
@@ -367,18 +367,20 @@ function DebugGui() {
 
 
   function onLoad() {
-    var script = document.createElement("script");
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js';
-    script.integrity = "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1";
-    script.setAttribute('crossorigin', 'anonymous');
-    document.head.appendChild(script);
+    if (debug()) {
+      var script = document.createElement("script");
+      script.src = 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js';
+      script.integrity = "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1";
+      script.setAttribute('crossorigin', 'anonymous');
+      document.head.appendChild(script);
 
-    script = document.createElement("script");
-    script.src = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js';
-    script.integrity = "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM";
-    script.setAttribute('crossorigin', 'anonymous');
-    document.head.appendChild(script);
-    init();
+      script = document.createElement("script");
+      script.src = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js';
+      script.integrity = "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM";
+      script.setAttribute('crossorigin', 'anonymous');
+      document.head.appendChild(script);
+      init();
+    }
   }
 
 
