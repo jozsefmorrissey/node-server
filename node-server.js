1c04f9c749384a50f960bdec53dd522fd458dd7f
Jozsef Morrissey <jozsef.morrissey@gmail.com>
2020-12-17 17:48:32 -0600

    modified cors policy

--- node-server.js
+++ node-server.js
@@ -50,7 +50,7 @@ var https_options = {
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
