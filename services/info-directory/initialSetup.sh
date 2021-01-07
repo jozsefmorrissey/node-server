7ddbfe464ddeeb06308d5a76f7064a7c9799317d
Jozsef Morrissey <jozsef.morrissey@gmail.com>
2021-01-06 18:00:04 -0600

    forgot to save initial setup changes info-dir

--- services/info-directory/initialSetup.sh
+++ services/info-directory/initialSetup.sh
@@ -5,7 +5,7 @@ pstPin=$(( $RANDOM % 10 + 1 ))$(( $RANDOM % 10 + 1 ))$(( $RANDOM % 10 + 1 ))$(( 
 
 dataDir=~/.opsc/info-directory/ && mkdir -p "$dataDir"
 
-echo '[{"Title": "", "Description": "", "Link" "id": 0}]' > "$dataDir/info-directory.json"
+echo '[{"Title": "Introduction", "Description": "Vision for my work and videos", "Link": "", "id": 0}]' > "$dataDir/info-directory.json"
 echo '{"Introduction": {"votes": 0}}' > "$dataDir/requested-topics.json"
 
 pst update $group pst-pin $(echo $pstPin)
