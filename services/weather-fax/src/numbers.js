685a68b6141fc544d8c1644da0f70338a622f519
Jozsef Morrissey <jozsef.morrissey@gmail.com>
2022-05-15 22:28:53 -0500

    fixing compilation error

--- services/weather-fax/src/numbers.js
+++ services/weather-fax/src/numbers.js
@@ -11,13 +11,13 @@ class FaxNumber {
 }
 
 
-numbers.push(new FaxNumber('Weather Request', '2178171112',
+NUMBERS.push(new FaxNumber('Weather Request', '2178171112',
   'Fax this number anything to recieve a weather report, whateverr you send will be disgaurded.'));
 
-numbers.push(new FaxNumber('Account Service', '2175729339',
+NUMBERS.push(new FaxNumber('Account Service', '2175729339',
   'This number will automatically send an account Information form that you can update and change all materials submitted to this number will be reviewed by a person'));
 
-numbers.push(new FaxNumber('Toggle Schedualed Reports', '2028732223',
+NUMBERS.push(new FaxNumber('Toggle Schedualed Reports', '2028732223',
   'This number allowed you to turn your schedualed reports on and off.'));
 
 module.exports = NUMBERS;
