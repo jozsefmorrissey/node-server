1d7c5b21c02851edb9e281c5ae7bf5c352afe499
Jozsef Morrissey <jozsef.morrissey@gmail.com>
2023-10-31 18:08:36 -0500

    again

--- services/cabinet/generated/EPNTS.js
+++
@@ -1,3 +0,0 @@
-const Endpoints = require('../../../public/js/utils/endpoints.js');
-const json = require('../public/json/endpoints.json');
-module.exports = new Endpoints(json, 'local').getFuncObj();
