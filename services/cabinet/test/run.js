00a95b5f4be48e81be416221f9f95add507a9e82
daxterix <daxterix3000@gmail.com>
2024-01-13 16:59:14 -0500

    test: reenable other tests

--- services/cabinet/test/run.js
+++ services/cabinet/test/run.js
@@ -3,22 +3,22 @@ const Test = require('../../../public/js/utils/test/test').Test;
 
 if (EPNTS.getEnv() === 'local') {
   require('./tests/web-worker');
-  // require('../../../public/js/utils/test/tests/vector3D');
-  // require('../../../public/js/utils/test/tests/polygon3d');
-  // require('../../../public/js/utils/test/tests/navigator');
-  // require('../../../public/js/utils/test/tests/compress-string');
-  // require('../../../public/js/utils/test/tests/json-utils');
-  // require('../../../public/js/utils/test/tests/utils');
-  // require('../../../public/js/utils/test/tests/imposter');
-  // require('./tests/cabinet');
-  // // TODO: write new tests - not important until we start working on the programmable cost interface.
-  // // require('../../../public/js/utils/test/tests/decision-tree');
-  // require('../../../public/js/utils/test/tests/star-line-map');
-  // require('./tests/polygon-merge');
-  // require('./tests/array-math');
-  // require('./tests/plane');
-  // require('./tests/line2d');
-  // require('./tests/line-consolidate');
+  require('../../../public/js/utils/test/tests/vector3D');
+  require('../../../public/js/utils/test/tests/polygon3d');
+  require('../../../public/js/utils/test/tests/navigator');
+  require('../../../public/js/utils/test/tests/compress-string');
+  require('../../../public/js/utils/test/tests/json-utils');
+  require('../../../public/js/utils/test/tests/utils');
+  require('../../../public/js/utils/test/tests/imposter');
+  require('./tests/cabinet');
+  // TODO: write new tests - not important until we start working on the programmable cost interface.
+  // require('../../../public/js/utils/test/tests/decision-tree');
+  require('../../../public/js/utils/test/tests/star-line-map');
+  require('./tests/polygon-merge');
+  require('./tests/array-math');
+  require('./tests/plane');
+  require('./tests/line2d');
+  require('./tests/line-consolidate');
 }
 
 Test.run();
